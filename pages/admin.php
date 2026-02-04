6e725edfe0a427fc2595d54e2bb3d1eca469e233
EarlConz <constanteearl@gmail.com>
2026-02-04 09:15:56 +0800

    Revert "Setup for Apache Service"
    This reverts commit 21bf0b69ad983cec694cf0ea695d0e7070e8483e.

--- pages/admin.php
+++ pages/admin.php
@@ -1,5 +1,5 @@
 <?php
-  require dirname(__DIR__, 2) . "/bootstrap.php";
+  require "../bootstrap.php";
 
   $pageTitle = "Administrator";
   $pageSubtitle = date("l, F j, Y");
--- pages/logout.php
+++ pages/logout.php
@@ -1,5 +1,5 @@
 <?php
-  require dirname(__DIR__, 2) . "/bootstrap.php";
+  require "../bootstrap.php";
   logout_user();
   header("Location: login.php");
   exit;
